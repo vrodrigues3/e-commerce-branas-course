a5549902daad89fc63ed487490b3f337c19582d8
vrodrigues3 <vrodrigues@tractian.com>
2023-12-28 14:16:03 -0300

    feat: created Checkout class

---
+++ src/Checkout.ts
@@ -0,0 +1,67 @@
+import { CouponData } from './CouponData'
+import { ProductData } from './ProductData'
+import { validate } from './cpfValidator'
+
+type Input = {
+  cpf: string
+  items: {
+    idProduct: number
+    quantity: number
+  }[]
+  coupon?: string
+}
+
+export class Checkout {
+  constructor(
+    readonly productData: ProductData,
+    readonly couponData: CouponData
+  ) {}
+
+  async execute(input: Input) {
+    const isValid = validate(input.cpf)
+    if (!isValid) {
+      throw new Error('Invalid cpf')
+    }
+
+    let total = 0
+    let freight = 0
+
+    const products: number[] = []
+
+    for (const item of input.items) {
+      if (products.some((idProduct) => idProduct === item.idProduct)) {
+        throw new Error('Duplicated product')
+      }
+      products.push(item?.idProduct)
+      const product = await this.productData.getProduct(item.idProduct)
+      if (product) {
+        if (item.quantity <= 0) {
+          throw new Error('Quantity must be positive')
+        }
+
+        total += product.price * item.quantity
+        const volume =
+          (product.width / 100) *
+          (product.height / 100) *
+          (product.length / 100)
+        const density = product.weight / volume
+        const itemFreight = 1000 * volume * (density / 100)
+        freight += itemFreight >= 10 ? itemFreight : 10
+      } else {
+        throw new Error('Product not found')
+      }
+    }
+
+    if (input.coupon) {
+      const coupon = await this.couponData.getCoupon(input.coupon)
+      const today = new Date()
+      if (coupon && coupon.expireDate.getTime() > today.getTime()) {
+        total -= (total * coupon.percentage) / 100
+      }
+    }
+
+    total += freight
+
+    return { total }
+  }
+}
