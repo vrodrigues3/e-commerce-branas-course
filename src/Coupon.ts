77e2761bcda2dd245d68513762ec6960dd292a1f
vrodrigues3 <vrodrigues@tractian.com>
2023-12-29 16:19:39 -0300

    feat: coupon class with isExpired and get discount methods

---
+++ src/Coupon.ts
@@ -0,0 +1,16 @@
+export class Coupon {
+  constructor(
+    readonly code: string,
+    readonly percentage: number,
+    readonly expireDate: Date
+  ) {}
+
+  isExpired() {
+    const today = new Date()
+    return this.expireDate.getTime() < today.getTime()
+  }
+
+  getDiscount(total: number) {
+    return (total * this.percentage) / 100
+  }
+}
