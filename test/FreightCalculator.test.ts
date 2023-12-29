81e4906b0901bfea5adab07ae113aba0712a0832
vrodrigues3 <vrodrigues@tractian.com>
2023-12-29 16:18:36 -0300

    test: calculate freight method

---
+++ test/FreightCalculator.test.ts
@@ -0,0 +1,23 @@
+import { FreightCalculator } from '../src/FreightCalculator'
+
+test('should calculate freight', () => {
+  const product = {
+    width: 100,
+    height: 30,
+    length: 10,
+    weight: 3
+  }
+  const freight = FreightCalculator.calculate(product)
+  expect(freight).toBe(30)
+})
+
+test('should calculate minimum freight', () => {
+  const product = {
+    width: 10,
+    height: 10,
+    length: 10,
+    weight: 0.9
+  }
+  const freight = FreightCalculator.calculate(product)
+  expect(freight).toBe(10)
+})
